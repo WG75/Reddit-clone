de3afef05b380ffd67a9be1cfd87ecf63e09f58e
Doubleu Gee <Waleed.gamal1000@gmail.com>
2017-03-12 05:41:22 +0200

    just getting started on the project

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+(function(){
+
+//listen for the dom and when it's loaded excute fun (excuteCode)
+  if(document.addEventListener){
+    document.addEventListener('DOMContentLoaded', excuteCode);
+  }else{
+//workaround for older browsers that don't support 'DOMContentLoaded' event
+    document.onreadystatechange = function(){
+      if(document.readyState === 'interactive'){
+        excuteCode();
+      }
+    }
+  }
+
+
+//contains all the functions, idon't know what to call this!
+  function excuteCode(){
+    
+  }
+
+})();
