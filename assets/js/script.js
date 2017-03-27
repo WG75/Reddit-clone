15bfbb7f21590027fcea1601a5a5774d744e74d5
Doubleu Gee <Waleed.gamal1000@gmail.com>
2017-03-27 16:50:39 +0200

    finished the markup of the post template

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,36 @@
 (function(){
 
+  "use strict";
+
 //listen for the dom and when it's loaded excute fun (excuteCode)
-  if(document.addEventListener){
-    document.addEventListener('DOMContentLoaded', excuteCode);
-  }else{
-//workaround for older browsers that don't support 'DOMContentLoaded' event
-    document.onreadystatechange = function(){
-      if(document.readyState === 'interactive'){
-        excuteCode();
-      }
-    }
-  }
+  document.addEventListener('DOMContentLoaded', excuteCode);
 
-
-//contains all the functions, idon't know what to call this!
   function excuteCode(){
-    console.log('it\'s working');
+
+    var reddit = {
+      url: 'hot',
+      
+    }
+
+
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 })();
